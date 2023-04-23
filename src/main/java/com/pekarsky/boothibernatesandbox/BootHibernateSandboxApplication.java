a54dcc0b72d33ce64b45332b041a230b825b2d48
sergii.pekarskyi <sergii.pekarskyi@hdsglobal.com>
2023-04-23 12:41:59 +0200

    Initial commit

---
+++ src/main/java/com/pekarsky/boothibernatesandbox/BootHibernateSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.pekarsky.boothibernatesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootHibernateSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootHibernateSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pekarsky/boothibernatesandbox/BootHibernateSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pekarsky.boothibernatesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootHibernateSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
